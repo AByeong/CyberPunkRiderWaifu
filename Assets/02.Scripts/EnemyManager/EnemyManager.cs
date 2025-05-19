02520c5f13bec0f2d04a59705bc7c49ffe4dadc0
AByeong <106604214+AByeong@users.noreply.github.com>
2025-05-19 17:13:26 +0900

    시네머신
    - 시네머신 패키지를 이용해서 시퀀스를 만드는 것에 성공했습니다
    - Dummy를 만들었습니다. 여럿 테스트할 때 쓰일 예정입니다.

--- Assets/02.Scripts/EnemyManager/EnemyManager.cs
+++ Assets/02.Scripts/EnemyManager/EnemyManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.Serialization;
 
 public class EnemyManager : Singleton<EnemyManager>
@@ -15,16 +16,10 @@ public class EnemyManager : Singleton<EnemyManager>
    
    [SerializeField]
    private KillTracker _killTracker;
+
+
+   public PlayableDirector CinemachineDrector;
    
-   void Update()
-   {
-      if (Input.GetKey(KeyCode.Space))
-      {
-         InitSpawn();
-      }
-      
-      Time.timeScale = Timer;
-   }
 
    private void InitSpawn()
    {
