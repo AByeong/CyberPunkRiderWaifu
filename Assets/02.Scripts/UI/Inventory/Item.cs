cf23a2884e97afff5131604294bf536fdd1c3105
GuardKim <kimkyoungho0726@gmail.com>
2025-05-22 13:47:42 +0900

    EquipmentSlot 수정, EquipmentData 수정, Inventory, InventorySlot 수정, Item 수정

--- Assets/02.Scripts/UI/Inventory/Item.cs
+++ Assets/02.Scripts/UI/Inventory/Item.cs
@@ -4,8 +4,11 @@ public enum ItemType {Equipment, Consumable, Etc}
 [System.Serializable]
 public class Item
 {
+    public string Id;
     public string ItemName;
     public Sprite Icon;
-    public ItemType ItmeType;
+    public ItemType ItemType;
+
+    public string EquipmentDataId;
     public EquipmentData EquipmentData;
 }
