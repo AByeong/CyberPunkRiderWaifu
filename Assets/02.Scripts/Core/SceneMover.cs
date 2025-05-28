1ad24ee3960e1dec14df85b548b0571942c25f97
AByeong <106604214+AByeong@users.noreply.github.com>
2025-05-28 09:58:56 +0900

    Bike Animation Asset Import

--- Assets/02.Scripts/Core/SceneMover.cs
+++ Assets/02.Scripts/Core/SceneMover.cs
@@ -3,9 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class SceneMover : Singleton<SceneMover>
 {
+
+
+    public void ShowDungeonPopup(string dungeonName)
+    {
+        //UIManager.Instance.PopupManager.PopupStack.Clear();
+        
+        
+        UIManager.Instance.PopupManager.ShowAnswerPopup("배달하러 가시겠습니까?","당근 빳따죠!","아, 잠만요",() =>
+        {
+            UIManager.Instance.PopupManager.CloseAllPopups();
+            
+            //이제 던전에 들어가고 커서 락이 필요함
+            UIManager.Instance.isInDelivery = true;
+            UIManager.Instance.isCursorLockNeed = true;
+            
+            SceneManager.LoadScene(dungeonName);
+           
+        },numberofbuttons : 2);
+
+    }
+    
+    
     public void MovetoScene(string sceneName)
     {
+        
         UIManager.Instance.PopupManager.PopupStack.Clear();
         SceneManager.LoadScene(sceneName);
+
     }
 }
