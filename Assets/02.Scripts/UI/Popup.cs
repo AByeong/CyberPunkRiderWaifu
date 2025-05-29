01e4f1092d84b681ddf9999bbb4d204defb9d7b0
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2025-05-29 10:00:20 +0900

    스킬 오류 해결

--- Assets/02.Scripts/UI/Popup.cs
+++ Assets/02.Scripts/UI/Popup.cs
@@ -1,25 +1,22 @@
-using System;
 using UnityEngine;
-
 public abstract class Popup : MonoBehaviour
 {
-    
-    virtual public void OpenPopup() // 매개변수 없음
+
+    public virtual void OpenPopup() // 매개변수 없음
     {
-        
-        UIManager.Instance.PopupManager.PopupStack.Push(this); 
-        
-        this.gameObject.SetActive(true);
-        
-        
-        
+
+        UIManager.Instance.PopupManager.PopupStack.Push(this);
+
+        gameObject.SetActive(true);
+
+
         UIManager.Instance.CursorLock(false);
-        
-      //  Debug.Log($"현재 {UIManager.Instance.PopupManager.PopupStack.Count}개의 팝업이 열려있음");
+
+        //  Debug.Log($"현재 {UIManager.Instance.PopupManager.PopupStack.Count}개의 팝업이 열려있음");
         if (UIManager.Instance.PopupManager.PopupStack.Count > 0)
         {
             string popuplist = "";
-            foreach (var popup in UIManager.Instance.PopupManager.PopupStack)
+            foreach(Popup popup in UIManager.Instance.PopupManager.PopupStack)
             {
                 popuplist += popup.name + ", ";
             }
@@ -29,38 +26,35 @@ public abstract class Popup : MonoBehaviour
 
     // 팝업이 닫힐 때 호출될 추상/가상 메서드를 추가합니다.
     // 자식 클래스에서 이 메서드를 오버라이드하여 각자의 닫힘 로직을 구현합니다.
-    protected virtual void OnPopupClosed() 
+    protected virtual void OnPopupClosed()
     {
         UIManager.Instance.CursorLock(true);
-        
+
         GameManager.Instance.GameReplay();
         UIManager.Instance.ESCisClose = false;
     }
 
-    virtual public void ClosePopup()
+    public virtual void ClosePopup()
     {
-        
-        
-        
-        
-        
+
+
 //        Debug.Log($"닫는순간 현재 {UIManager.Instance.PopupManager.PopupStack.Count}개의 팝업이 열려있음");
-        
+
         if (UIManager.Instance.PopupManager.PopupStack.Count > 0)
         {
             string popuplist = "";
-            foreach (var popup in UIManager.Instance.PopupManager.PopupStack)
+            foreach(Popup popup in UIManager.Instance.PopupManager.PopupStack)
             {
                 popuplist += popup.name + ", ";
             }
             Debug.Log(popuplist);
         }
-        
+
         if (UIManager.Instance.PopupManager.PopupStack.Count == 0)
         {
             OnPopupClosed();
         }
-        
+
         gameObject.SetActive(false);
     }
 }
