b3fc8944bbb42fa5192d08c65e060015bbc189ae
AByeong <106604214+AByeong@users.noreply.github.com>
2025-05-30 10:46:30 +0900

    여성형 보스 공격모션

--- Assets/02.Scripts/Enemies/StateMachine/EliteEnemy/EliteAttackState.cs
+++ Assets/02.Scripts/Enemies/StateMachine/EliteEnemy/EliteAttackState.cs
@@ -4,6 +4,7 @@ public class EliteAttackState : EliteBaseState
 {
     private float _attackTimer;
 
+    private bool _isAttacking = false;
     public override void OnEnter()
     {
         base.OnEnter();
@@ -13,12 +14,22 @@ public class EliteAttackState : EliteBaseState
         Owner.AttackType = Random.Range(0, 3);
     }
 
+    public void AttackStart()
+    {
+        _isAttacking = true;
+    }
+
+    public void AttackEnd()
+    {
+        _isAttacking = false;
+    }
+    
     public override void Update()
     {
         base.Update();
 
         float distance = Vector3.Distance(Owner.transform.position, Owner.Target.transform.position);
-        if (distance >= Owner.EnemyData.AttackDistance)
+        if (distance >= Owner.EnemyData.AttackDistance && !_isAttacking)
         {
             SuperMachine.ChangeState<EliteIdleState>();
             return;
