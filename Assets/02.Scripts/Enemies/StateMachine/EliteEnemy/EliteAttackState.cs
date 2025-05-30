f97b8d1944d01f7f7e3dc66bad276cf98f2c655a
kencoor9 <kencoor9@gmail.com>
2025-05-30 17:06:59 +0900

    애니메이션 오류 해결

--- Assets/02.Scripts/Enemies/StateMachine/EliteEnemy/EliteAttackState.cs
+++ Assets/02.Scripts/Enemies/StateMachine/EliteEnemy/EliteAttackState.cs
@@ -2,24 +2,28 @@ using UnityEngine;
 using UnityEngine.Serialization;
 
 public class EliteAttackState : EliteBaseState
-{    
+{
     public override void OnEnter()
     {
         base.OnEnter();
         Owner.NavMeshAgent.enabled = false;
 
-        
+
     }
 
-    
-    
+
+
     public override void Update()
     {
         base.Update();
 
-        
+
+       
+    }
+
+    private void LateUpdate()
+    {
         float distance = Vector3.Distance(Owner.transform.position, Owner.Target.transform.position);
-        
 
         if (distance >= Owner.EnemyData.AttackDistance && !Owner.IsAttacking)
         {
@@ -27,16 +31,14 @@ public class EliteAttackState : EliteBaseState
             return;
         }
 
-        
+
         if (Owner.AttackTimer >= Owner.EnemyData.AttackCoolDown && !Owner.IsAttacking)
         {
             Owner.IsAttacking = true;
             Owner.AttackType = Random.Range(0, 3);
             Owner.Animator.SetTrigger("OnAttack");
         }
-        
-        
     }
 
-    
+
 }
