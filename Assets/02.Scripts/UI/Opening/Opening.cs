f27dc473fb2f97be9b80de516e9791226e6110df
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2025-05-31 19:42:10 +0900

    시작씬 수정
    필살기 연출 시작

--- Assets/02.Scripts/UI/Opening/Opening.cs
+++ Assets/02.Scripts/UI/Opening/Opening.cs
@@ -1,40 +1,41 @@
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class Opening : MonoBehaviour
 {
-    public Image targetImage; // Inspector에서 할당하거나 코드로 연결
-    public float fadeDuration = 2f; // n초 (페이드 인 지속 시간)
+    public Image TargetImage; // Inspector에서 할당하거나 코드로 연결
+    public float FadeDuration = 2f; // n초 (페이드 인 지속 시간)
     public GameObject StartMenuUI;
 
     public void OffImage()
     {
-        targetImage.enabled = false;
+        TargetImage.enabled = false;
         StartMenuUI.gameObject.SetActive(false);
     }
 
     public void FadeInImage()
     {
-        targetImage.enabled = true;
-        if (targetImage == null)
+        TargetImage.enabled = true;
+        if (TargetImage == null)
         {
             Debug.LogWarning("Image is null.");
             return;
         }
 
         // 처음에는 완전 투명
-        Color color = targetImage.color;
+        Color color = TargetImage.color;
         color.a = 0f;
-        targetImage.color = color;
+        TargetImage.color = color;
 
         // DOFade를 통해 alpha를 1로 n초 동안 서서히 변경
-        targetImage.DOFade(1f, fadeDuration).OnComplete(() => { StartMenuUI.SetActive(true); });
+        TargetImage.DOFade(1f, FadeDuration).OnComplete(() => { StartMenuUI.SetActive(true); });
     }
 
     public void GameStart()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("Loading");
     }
 }
