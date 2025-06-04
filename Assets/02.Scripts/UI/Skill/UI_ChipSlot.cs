e3ec12de03149f2b8a029f59d94879fa9da0c2c3
GuardKim <kimkyoungho0726@gmail.com>
2025-06-04 17:10:10 +0900

    UI_ChipSlot

--- Assets/02.Scripts/UI/Skill/UI_ChipSlot.cs
+++ Assets/02.Scripts/UI/Skill/UI_ChipSlot.cs
@@ -1,12 +1,25 @@
-﻿public class UI_ChipSlot : UI_InventorySlot
+﻿using System;
+using System.Drawing;
+using UnityEngine.UI;
+using Color = UnityEngine.Color;
+
+public class UI_ChipSlot : UI_InventorySlot
 {
     public UI_EquipSkill TargetSkill;
+    private Image _slotImage;
+
+    private void Awake()
+    { 
+        _slotImage = GetComponent<Image>();
+    }
 
     public override void SetItem(Item item)
     {
         base.SetItem(item);
         if (TargetSkill != null)
         {
+            if(_slotImage != null)
+                _slotImage.color = new Color(_slotImage.color.r, _slotImage.color.g, _slotImage.color.b, 1f);
             TargetSkill.RefreshChipEffects();
         }
     }
@@ -16,6 +29,8 @@
         base.ClearItem();
         if (TargetSkill != null)
         {
+            if(_slotImage != null)
+                _slotImage.color = new Color(_slotImage.color.r, _slotImage.color.g, _slotImage.color.b, 0f);
             TargetSkill.RefreshChipEffects();
         }
     }
