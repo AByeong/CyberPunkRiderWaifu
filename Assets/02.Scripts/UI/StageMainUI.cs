71d0e2e8be04c289a3f20223255221c1b91b7350
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2025-06-05 16:28:48 +0900

    Item 이미지 수정

--- Assets/02.Scripts/UI/StageMainUI.cs
+++ Assets/02.Scripts/UI/StageMainUI.cs
@@ -83,7 +83,6 @@ public class StageMainUI : MonoBehaviour
             }
             else
             {
-                SkillIcons[i].IconImage.sprite = null;
                 SkillIcons[i].RestrictCondition = -1;
             }
         }
